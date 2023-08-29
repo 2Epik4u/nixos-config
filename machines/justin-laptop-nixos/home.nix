a7026810bb399772650362d2380f61ff0298befc
2Epik4u <justinabossmlg@gmail.com>
2023-08-29 13:59:45 -0400

    swaylock should only be on laptop

--- machines/justin-laptop-nixos/home.nix
+++ machines/justin-laptop-nixos/home.nix
@@ -4,6 +4,8 @@
   imports = [
       # import the global one
         ./../../home.nix
+        # swayidle laptop only
+        ./../../wayland/swayidle.nix
   ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
--- wayland/default.nix
+++ wayland/default.nix
@@ -3,11 +3,10 @@
     inputs,
     ...
 }: {
-  imports = [./hyprland ./waybar ./fuzzel.nix ./swayidle.nix ];
+  imports = [./hyprland ./waybar ./fuzzel.nix  ];
 
 home.packages = with pkgs; [
     fuzzel
-    wineWowPackages.wayland
     swaybg
 ];
 }
